50feaac5bbbb32d39c614298d3221befe3d4e375
Aman Kumar Sahani <71867895+amankumarsahani@users.noreply.github.com>
2024-08-26 15:50:20 +0530

    sign up form structure and style updated

--- src/components/User/Register.js
+++ src/components/User/Register.js
@@ -1,6 +1,9 @@
 import React, { useState, useContext } from 'react';
 import { AuthContext } from 'context/AuthContext';
-import { Card, CardBody, CardHeader, CardTitle, Col, Row } from 'reactstrap';
+import { Link } from "react-router-dom";
+import emailIcon from '../../assets/img/email.svg';
+import userIcon from '../../assets/img/user.svg';
+import passwordIcon from '../../assets/img/password.svg';
 
 const Register = () => {
   console.log("jiii");
@@ -18,26 +21,32 @@ const Register = () => {
 
   return (
     <>
-      <div className="content">
-        <Row>
-          <Col lg="3" md="6" sm="6">
-            <Card className="card-stats">
-              <CardHeader>
-                <CardTitle tag="h4">Campaigns</CardTitle>
-              </CardHeader>
-              <CardBody>
-                <form onSubmit={handleSubmit}>
-                  <input type="text" name="username" placeholder="Username" onChange={handleChange} required />
-                  <input type="email" name="email" placeholder="Email" onChange={handleChange} required />
-                  <input type="password" name="password" placeholder="Password" onChange={handleChange} required />
-                  <input type="text" name="companyName" placeholder="Company Name" onChange={handleChange} required />
-                  <button type="submit">Register</button>
-                </form>
-              </CardBody>
-            </Card>
-          </Col>
-        </Row>
+    <main className="form-signin w-100 d-flex align-items-center justify-content-center" style={{minHeight:'100vh'}}>
+    <div className='sign-up-form-width shadow-sm p-3 mb-5 rounded border text-center' style={{backgroundColor:'#fff'}}>
+      <form onSubmit={handleSubmit}>
+        <p className="h3 mb-3 fw-bold primary-text-color">Sign Up</p>
+        <div className="my-2 input-wrapper">
+          <img src={userIcon} alt="User icon" />
+          <input type="text" className="form-control py-3 shadow-sm rounded" id="floatingName" placeholder="Username" onChange={handleChange} required/>
+        </div>
+        <div className="my-2 input-wrapper">
+          <img src={emailIcon} alt="Email icon" />
+          <input type="email" className="form-control py-3 shadow-sm rounded" id="floatingInput" placeholder="Email" onChange={handleChange} required/>
+        </div>
+        <div className="my-2 input-wrapper">
+          <img src={passwordIcon} alt="Password icon" />
+          <input type="password" className="form-control py-3 shadow-sm rounded" id="floatingPassword" placeholder="Password" onChange={handleChange} required/>
+        </div>
+        <div className="my-2 input-wrapper">
+          <img src={userIcon} alt="Company icon" />
+          <input type="text" className="form-control py-3 shadow-sm rounded" id="floatingCName" placeholder="Company Name" onChange={handleChange} required/>
+        </div>
+
+        <button className="btn btn-primary btn-lg w-100 py-2 mt-3" type="submit">Create Account</button>
+        <p className=''>Already have a account? <Link to="/login">Sign Up</Link></p>
+      </form>
       </div>
+    </main>
     </>
   );
 };
