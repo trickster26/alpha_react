085c0a8ca1479ce2ddcb5f40b6a1ac0c8a891493
ANU KUMAR <68609057+trickster26@users.noreply.github.com>
2024-08-29 08:10:52 +0530

    authentication and error handling

--- src/components/User/Login.js
+++ src/components/User/Login.js
@@ -1,54 +1,107 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect, useRef } from 'react';
 import { AuthContext } from 'context/AuthContext';
 import emailIcon from '../../assets/img/email.svg';
 import passwordIcon from "../../assets/img/password.svg";
 import styles from "../../assets/css/SignUp.module.css";
-// import { ToastContainer } from "react-toastify";
-// import "react-toastify/dist/ReactToastify.css";
 import { Link } from "react-router-dom";
+import NotificationAlert from "react-notification-alert";
 
 const Login = () => {
-  const { login } = useContext(AuthContext);
+  const { login, loading, error, setError } = useContext(AuthContext);
   const [formData, setFormData] = useState({ email: '', password: '' });
+  const [errors, setErrors] = useState({});
   const [touched, setTouched] = useState({});
+  const notificationAlert = useRef(null);
+
+  const validate = (name, value) => {
+    let error = '';
+
+    if (name === 'email') {
+      if (!value) {
+        error = 'Email is required';
+      } else if (!/\S+@\S+\.\S+/.test(value)) {
+        error = 'Email address is invalid';
+      }
+    }
+
+    if (name === 'password') {
+      if (!value) {
+        error = 'Password is required';
+      } else if (value.length < 6) {
+        error = 'Password must be at least 6 characters';
+      }
+    }
+
+    return error;
+  };
 
   const handleChange = (e) => {
-    if (e.target.name === "IsAccepted") {
-      setFormData({ ...formData, [e.target.name]: e.target.checked });
-    } else {
-      setFormData({ ...formData, [e.target.name]: e.target.value });
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+
+    if (touched[name]) {
+      const error = validate(name, value);
+      setErrors({ ...errors, [name]: error });
     }
   };
 
-  const handleSubmit = (e) => {
+  const handleBlur = (e) => {
+    const { name, value } = e.target;
+    setTouched({ ...touched, [name]: true });
+
+    const error = validate(name, value);
+    setErrors({ ...errors, [name]: error });
+  };
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    login(formData.email, formData.password);
+    
+    const emailError = validate('email', formData.email);
+    const passwordError = validate('password', formData.password);
+
+    if (emailError || passwordError) {
+      setErrors({ email: emailError, password: passwordError });
+      return;
+    }
+
+    try {
+      await login(formData.email, formData.password);
+    } catch (error) {
+      // Handle error within the context if needed
+    }
   };
 
-  const focusHandler = (event) => {
-    setTouched({ ...touched, [event.target.name]: true });
+  const showNotification = (message) => {
+    const options = {
+      place: 'tr',
+      message: (
+        <div>
+          <div>{message}</div>
+        </div>
+      ),
+      type: 'danger',
+      icon: "nc-icon nc-bell-55",
+      autoDismiss: 7,
+    };
+    notificationAlert.current.notificationAlert(options);
   };
 
-  
-// const chaeckData = (obj) => {
-//   const { email, password } = obj;
-//   const urlApi = `https://lightem.senatorhost.com/login-react/index.php?email=${email.toLowerCase()}&password=${password}`;
-//   const api = axios
-//     .get(urlApi)
-//     .then((response) => response.data)
-//     .then((data) => (data.ok ? notify("You login to your account successfully", "success") : notify("Your password or your email is wrong", "error")));
-//   toast.promise(api, {
-//     pending: "Loading your data...",
-//     success: false,
-//     error: "Something went wrong!",
-//   });
-// };
-
+  useEffect(() => {
+    console.log(error)
+    if (error.length > 0) {
+      showNotification(error);
+      setTimeout(() => {
+        setError('');
+      }, 1500);  // Clear the error after 1.5 seconds
+    }
+  }, [error, setError]);
 
   return (
     <div className={styles.container}>
-    <form className={styles.formLogin} autoComplete="off" onSubmit={handleSubmit}>
-      <h2>Sign In</h2>
+      <NotificationAlert ref={notificationAlert} />
+      <form className={styles.formLogin} autoComplete="off" onSubmit={handleSubmit}>
+        <h2>Sign In</h2>
+
         <div>
           <div>
             <input 
@@ -57,10 +110,12 @@ const Login = () => {
               value={formData.email} 
               placeholder="E-mail" 
               onChange={handleChange} 
-              onFocus={focusHandler} 
+              onBlur={handleBlur} 
               autoComplete="off" 
+              disabled={loading}  // Disable input if loading
             />
             <img src={emailIcon} alt="" />
+            {touched.email && errors.email && <div className={styles.error}>{errors.email}</div>}
           </div>
         </div>
 
@@ -72,21 +127,25 @@ const Login = () => {
               value={formData.password} 
               placeholder="Password" 
               onChange={handleChange} 
-              onFocus={focusHandler} 
-              autoComplete="off" />
+              onBlur={handleBlur} 
+              autoComplete="off"
+              disabled={loading}  // Disable input if loading
+            />
             <img src={passwordIcon} alt="" />
+            {touched.password && errors.password && <div className={styles.error}>{errors.password}</div>}
           </div>
         </div>
       
         <div>
-          <button type="submit">Login</button>
+          <button type="submit" disabled={loading}>
+            {loading ? 'Logging in...' : 'Login'}
+          </button>
           <span style={{ color: "#a29494", textAlign: "center", display: "inline-block", width: "100%" }}>
-            Don't have a account? <Link to="/signup">Create account</Link>
+            Don't have an account? <Link to="/signup">Create account</Link>
           </span>
         </div>
   
-    </form>
-    {/* <ToastContainer /> */}
+      </form>
     </div>
   );
 };
--- src/components/User/Register.js
+++ src/components/User/Register.js
@@ -1,52 +1,138 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useRef, useEffect } from 'react';
 import { AuthContext } from 'context/AuthContext';
 import { Link } from "react-router-dom";
 import emailIcon from '../../assets/img/email.svg';
 import userIcon from '../../assets/img/user.svg';
 import passwordIcon from '../../assets/img/password.svg';
+import NotificationAlert from "react-notification-alert";
 
 const Register = () => {
-  console.log("jiii");
-  const { register } = useContext(AuthContext);
+  const { register, loading, error, setError } = useContext(AuthContext);
   const [formData, setFormData] = useState({ username: '', email: '', password: '', companyName: '' });
+  const [errors, setErrors] = useState({});
+  const notificationAlert = useRef();
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
+    setErrors({ ...errors, [e.target.name]: '' });
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    register(formData.username, formData.email, formData.password, formData.companyName);
+  const validate = () => {
+    const newErrors = {};
+    if (!formData.username) newErrors.username = 'Username is required';
+    if (!formData.email) newErrors.email = 'Email is required';
+    if (!formData.password) newErrors.password = 'Password is required';
+    if (!formData.companyName) newErrors.companyName = 'Company Name is required';
+    return newErrors;
   };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const validationErrors = validate();
+    if (Object.keys(validationErrors).length > 0) {
+      setErrors(validationErrors);
+      return;
+    }
+    setErrors({});
+    
+    try {
+      await register(formData.username, formData.email, formData.password, formData.companyName);
+      notify('success', 'Account created successfully!');
+    } catch (error) {
+      notify('danger', 'An error occurred. Please try again.');
+    }
+  };
+
+  const notify = (type, message) => {
+    const options = {
+      place: 'tc', // top center
+      message: (
+        <div>
+          <div>{message}</div>
+        </div>
+      ),
+      type: type,
+      autoDismiss: 7,
+    };
+    notificationAlert.current.notificationAlert(options);
+  };
+
+  useEffect(() => {
+    if (error) {
+      notify('danger', error);
+      setTimeout(() => {
+        // Clear the error after 1.5 seconds
+        setError('');
+      }, 1500);
+    }
+  }, [error, setError]);
 
   return (
     <>
-    <main className="form-signin w-100 d-flex align-items-center justify-content-center" style={{minHeight:'100vh'}}>
-    <div className='sign-up-form-width shadow-sm p-3 mb-5 rounded border text-center' style={{backgroundColor:'#fff'}}>
-      <form onSubmit={handleSubmit}>
-        <p className="h3 mb-3 fw-bold primary-text-color">Sign Up</p>
-        <div className="my-2 input-wrapper">
-          <img src={userIcon} alt="User icon" />
-          <input type="text" className="form-control py-3 shadow-sm rounded" id="floatingName" placeholder="Username" onChange={handleChange} required/>
+      <main className="form-signin w-100 d-flex align-items-center justify-content-center" style={{ minHeight: '100vh' }}>
+        <div className='sign-up-form-width shadow-sm p-3 mb-5 rounded border text-center' style={{ backgroundColor: '#fff' }}>
+          <NotificationAlert ref={notificationAlert} />
+          <form onSubmit={handleSubmit}>
+            <p className="h3 mb-3 fw-bold primary-text-color">Sign Up</p>
+            {errors.username && <div className="text-danger">{errors.username}</div>}
+            {errors.email && <div className="text-danger">{errors.email}</div>}
+            {errors.password && <div className="text-danger">{errors.password}</div>}
+            {errors.companyName && <div className="text-danger">{errors.companyName}</div>}
+            <div className="my-2 input-wrapper">
+              <img src={userIcon} alt="User icon" />
+              <input
+                type="text"
+                className="form-control py-3 shadow-sm rounded"
+                placeholder="Username"
+                name="username"
+                value={formData.username}
+                onChange={handleChange}
+                disabled={loading}
+              />
+            </div>
+            <div className="my-2 input-wrapper">
+              <img src={emailIcon} alt="Email icon" />
+              <input
+                type="email"
+                className="form-control py-3 shadow-sm rounded"
+                placeholder="Email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                disabled={loading}
+              />
+            </div>
+            <div className="my-2 input-wrapper">
+              <img src={passwordIcon} alt="Password icon" />
+              <input
+                type="password"
+                className="form-control py-3 shadow-sm rounded"
+                placeholder="Password"
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+                disabled={loading}
+              />
+            </div>
+            <div className="my-2 input-wrapper">
+              <img src={userIcon} alt="Company icon" />
+              <input
+                type="text"
+                className="form-control py-3 shadow-sm rounded"
+                placeholder="Company Name"
+                name="companyName"
+                value={formData.companyName}
+                onChange={handleChange}
+                disabled={loading}
+              />
+            </div>
+            <button className="btn btn-primary btn-lg w-100 py-2 mt-3" type="submit" disabled={loading}>
+              {loading ? 'Creating Account...' : 'Create Account'}
+            </button>
+            <p>Already have an account? <Link to="/login">Sign In</Link></p>
+          </form>
         </div>
-        <div className="my-2 input-wrapper">
-          <img src={emailIcon} alt="Email icon" />
-          <input type="email" className="form-control py-3 shadow-sm rounded" id="floatingInput" placeholder="Email" onChange={handleChange} required/>
-        </div>
-        <div className="my-2 input-wrapper">
-          <img src={passwordIcon} alt="Password icon" />
-          <input type="password" className="form-control py-3 shadow-sm rounded" id="floatingPassword" placeholder="Password" onChange={handleChange} required/>
-        </div>
-        <div className="my-2 input-wrapper">
-          <img src={userIcon} alt="Company icon" />
-          <input type="text" className="form-control py-3 shadow-sm rounded" id="floatingCName" placeholder="Company Name" onChange={handleChange} required/>
-        </div>
-
-        <button className="btn btn-primary btn-lg w-100 py-2 mt-3" type="submit">Create Account</button>
-        <p className=''>Already have a account? <Link to="/login">Sign Up</Link></p>
-      </form>
-      </div>
-    </main>
+      </main>
     </>
   );
 };
